0564d4347fa85033e62ff1fbbde339981cedbf01
MadarKrissBaltins <MadarsKrissBaltins>
2021-03-23 08:23:59 +0200

    3. uzdevums

---
+++ 03-show-many-elements/global.js
@@ -0,0 +1,8 @@
+const hideMeElements = document.querySelectorAll(".hide_me")
+const button = document.querySelector("#button")
+
+button.addEventListener('click', function(e) {
+    hideMeElements.forEach(function(element) {
+        element.style.display = "none";
+    })
+})
